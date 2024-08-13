a0a023f1544233f43c9e0f5676a11d0a7c554d4d
igprad01 <igustiputra99@gmail.com>
2024-08-13 14:07:14 +0700

    fix membership function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // define fuzzy library
 
-import {  } from "fuzzy";
+import { fuzzy } from "fuzzy";
 
-// definisikan variable
+// definisikan variable fuzzy kedinginan
 
 const temperature = new fuzzy.variable("temperature", [
-        new fuzzy.temp("")
-])
+        new fuzzy.temp("dingin", new fuzzy.Triangular(0, 0, 20)),
+        new fuzzy.temp("sedang", new fuzzy.Triangular(15,20,25))
+
+]);
+
 
 // besok dilanjutkan
