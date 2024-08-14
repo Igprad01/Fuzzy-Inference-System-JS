7a837fe6db6bb905ef36da5e016ec36ed54c6b80
igprad01 <igustiputra99@gmail.com>
2024-08-14 14:46:38 +0700

    add variable keanggotaan output and inisialisasi var fuzzy

--- src/index.js
+++ src/index.js
@@ -4,11 +4,28 @@ import { fuzzy } from "fuzzy";
 
 // definisikan variable keanggotaan fuzzy suhu ruangan
 
-const temperature = new fuzzy.variable("temperature", [
+const suhuRuangan = new fuzzy.variable("temperature", [
         new fuzzy.temp("dingin", new fuzzy.Triangular(0, 0, 25)),
         new fuzzy.temp("sejuk", new fuzzy.Triangular(26,28,30)),
         new fuzzy.temp("hangat", new fuzzy.Triangular(31,33,35)),
         new fuzzy.temp("panas", new fuzzy.Triangular(36, 38, 42))
 ]);
 
-// definisi variable keanggotaan temperature ac
+// definisi variable keanggotaan temperature AC
+
+const SuhuAC = new fuzzy.variable("AC", [
+        new fuzzy.temp("dingin", new fuzzy.Triangular(16,18,20)),
+        new fuzzy.temp("hangat", new fuzzy.Triangular(21,23, 25)),
+        new fuzzy.temp("panas", new fuzzy.Triangular(26, 28, 30))
+]);
+
+// inisialisasi system fuzzy
+const FuzzySystem = new fuzzy.system("AC", [suhuRuangan], [SuhuAC]);
+
+
+// rule fuzzy
+
+FuzzySystem = [
+        // disini rule nya
+];
+
