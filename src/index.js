cd6f81599a452e52918e3aa99f4246d1eb45af71
igprad01 <igustiputra99@gmail.com>
2024-08-14 21:28:44 +0700

    delete all kemudian ganti vanilla js fuzzy

--- src/index.js
+++ src/index.js
@@ -1,49 +1,10 @@
-// define fuzzy library
+const suhuRuangan = (TemperatureRuangan) => {
+        return {
+                dingin:Math.max(0, math.min(1,(25 - TemperatureRuangan)/ 5))
+        }
+};
 
-import { fuzzy } from "fuzzy";
-
-// definisikan variable keanggotaan fuzzy suhu ruangan
-
-const suhuRuangan = new fuzzy.variable("temperature", [
-        new fuzzy.temp("dingin", new fuzzy.Triangular(0, 0, 25)),
-        new fuzzy.temp("sejuk", new fuzzy.Triangular(26,28,30)),
-        new fuzzy.temp("hangat", new fuzzy.Triangular(31,33,35)),
-        new fuzzy.temp("panas", new fuzzy.Triangular(36, 38, 42))
-]);
-
-// definisi variable keanggotaan temperature AC
-
-const SuhuAC = new fuzzy.variable("AC", [
-        new fuzzy.temp("dingin", new fuzzy.Triangular(16,18,20)),
-        new fuzzy.temp("hangat", new fuzzy.Triangular(21,23, 25)),
-        new fuzzy.temp("panas", new fuzzy.Triangular(26, 28, 30))
-]);
-
-// inisialisasi system fuzzy
-const FuzzySystem = new fuzzy.system("AC", [suhuRuangan], [SuhuAC]);
-
-
-// rule fuzzy
-
-FuzzySystem.rules = [
-        new fuzzy.rule(
-                new fuzzy.AND(suhuRuangan.is("dingin")),
-                SuhuAC.is("panas")
-        ),
-        new fuzzy.rule(
-                new fuzzy.AND(suhuRuangan.is("sejuk")),
-                SuhuAC.is("panas")
-        ),
-        new fuzzy.rule(
-                new fuzzy.AND(suhuRuangan.is("hangat")),
-                SuhuAC.is("hangat")
-        ),
-        new fuzzy.rule(
-                new fuzzy.AND(suhuRuangan.is("panas")),
-                SuhuAC.is("dingin")
-        )
-];
-
-// proses inferensi 
-
+const SuhuAC = (AC) => {
+        Dingin:Math.max(0, Math.min(1, (20 - AC) / 4))
+}
 
