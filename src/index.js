8daf3ec6309ab7e5d8aba5a45c43798584aab3e9
igprad01 <igustiputra99@gmail.com>
2024-08-14 11:20:45 +0700

    add keanggotaan suhu ruangan

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 
 import { fuzzy } from "fuzzy";
 
-// definisikan variable fuzzy kedinginan
+// definisikan variable keanggotaan fuzzy suhu ruangan
 
 const temperature = new fuzzy.variable("temperature", [
-        new fuzzy.temp("dingin", new fuzzy.Triangular(0, 0, 20)),
-        new fuzzy.temp("sedang", new fuzzy.Triangular(15,20,25))
-
+        new fuzzy.temp("dingin", new fuzzy.Triangular(0, 0, 25)),
+        new fuzzy.temp("sejuk", new fuzzy.Triangular(26,28,30)),
+        new fuzzy.temp("hangat", new fuzzy.Triangular(31,33,35)),
+        new fuzzy.temp("panas", new fuzzy.Triangular(36, 38, 42))
 ]);
+
+// definisi variable keanggotaan temperature ac
